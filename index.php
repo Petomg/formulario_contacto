2fa50fa39ed6a256456c0a09736f583051b64c80
peto <petomg.pmg@gmail.com>
2019-10-28 22:59:57 -0300

    Agregado getters en ContactModel

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 	
+	date_default_timezone_set('America/Los_Angeles');
+	$date = date('Y-m-d h:i:s', time());
 
-	
+	require_once './models/contact_model.php';
+	$connect = new ContactModel();
+	$arr = $connect->get_inquiries();
+	$arr2 = $connect->get_users();
+
+	print_r($arr2);
 ?>
--- models/contact_model.php
+++ models/contact_model.php
@@ -3,16 +3,22 @@
 	Class ContactModel {
 
 		private $connectdb;
+		private $users;
+		private $inquiries;
 	
 		public function __construct(){
 			#TODO: Arreglar el tema este de los path, que calcule el path dinamicamente
 			require_once '/var/www/html/formulario_contacto/general/connection_db.php';
 
-			$connectInst = new ConnectionDB();
-			$this->connectdb = $connectInst->connection(); #La variable con mi instancia de PDO
+			$connect_inst = new ConnectionDB();
+			$this->connectdb = $connect_inst->connection(); #La variable con mi instancia de PDO
+
+			$this->users = array();
+			$this->inquiries = array();
 		}
 
-		public function setInquiry($inquiryVector){
+		#Setters
+		public function set_inquiry($inquiryVector){
 			$query = "INSERT INTO inquiries(user_id, title, type, message, inquirie_date) VALUES (:user_id, :title, :type, :message, :inquirie_date)";
 
 			#Se usa prepare y placeholders para intentar prevenir SQL injection.		  
@@ -21,13 +27,27 @@
 			$stmt->execute($inquiryVector);		  
 		}
 
-		public function setUser($userVector){
+		public function set_user($userVector){
 			$query = "INSERT INTO users(first_name, last_name, email, company) VALUES (:first_name, :last_name, :email, :company)";
 
-			$stmt = ($this->connectdb)->prepare($query);
+			$stmt = $this->connectdb->prepare($query);
 			
 			$stmt->execute($userVector);
 		}
+
+		#Getters
+		public function get_inquiries(){
+			$query = "SELECT * FROM inquiries";
+			$this->inquiries = $this->connectdb->query($query)->fetchAll(); #fetchAll() me va a dar un array con las filas;
+			return $this->inquiries;	
+		}
+
+		public function get_users(){
+			$query = "SELECT * FROM users";
+			$this->users = $this->connectdb->query($query)->fetchAll();
+			return $this->users;	
+		}
+
 
 	}
 ?>
